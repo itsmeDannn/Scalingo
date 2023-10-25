f5b4a485df165d2d4d5ad86455d761fc305fe05e
D <56791595+iamclowningboy@users.noreply.github.com>
2023-10-25 13:48:57 +0700

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/iamclowningboy/File-bot ok && cd ok && pip3 install -U -r requirements.txt
-python3 main.py
+git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
+$START_CMD
