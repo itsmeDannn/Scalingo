10fe26d7f07a82d112d1ad32f26ece7028046202
D <56791595+iamclowningboy@users.noreply.github.com>
2023-10-25 20:07:17 +0700

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,2 @@
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
+git clone $REPO_URL ok && cd ok && pip install --upgrade pip && pip3 install -U -r requirements.txt
 $START_CMD
