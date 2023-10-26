b83a016a46e22e32f101594ca656405403caa368
Dannn <148627443+itsmeDannn@users.noreply.github.com>
2023-10-26 11:44:24 +0700

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,2 @@
-git clone $REPO_URL ok && cd ok && pip install --upgrade pip && npm i -g npm && pip3 install -U -r requirements.txt
+git clone $REPO_URL ok && cd ok && pip install --upgrade pip && pip3 install -U -r requirements.txt
 $START_CMD
